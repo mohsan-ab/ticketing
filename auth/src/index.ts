9d77ffeecd576455de7c498f82cfa965a3233656
mohsanabbas <moabbas.ch@gmail.com>
2020-10-15 22:49:59 -0300

    configured ingress nginx dev mode

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 const app = express();
 app.use(json());
+
+app.get('/api/users/currentuser', (req: Request, res: Response) => {
+  const dummyRequest = req.body;
+  console.log(dummyRequest);
+  res.send('test user');
+});
 app.listen(3000, () => {
   console.log('check listning on port 3000!');
 });
